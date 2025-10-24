5b2c208006881ecde329ce8e3deb1d3c47255148
DrRuxton <71925350+DrRuxton@users.noreply.github.com>
2025-10-24 10:16:37 +0200

    ToDo Kommentare hinzugefügt

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
 
+//ToDo pins als Variablen hinterlegen
+
 void setup() {
+  //ToDo Pin Deklarationen (evtl. automatisiert)
   
 }
 
 void loop() {
+  //Programmablauf einbauen
 
 }
