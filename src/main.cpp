21e6e7a7672ecaa9461ac9a67e86f7f8a5394fcd
DrRuxton <arthurmarcard@outlook.de>
2026-01-19 20:50:10 +0100

    Viel scheise gemacht; hat wenig gebracht; Ich hab keinen Plan, was zu Hölle da nicht geht; Hilfe!

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,20 @@
 #include <Arduino.h>
+#include <TFT_eSPI.h>
+
+TFT_eSPI tft = TFT_eSPI();
 
 //ToDo pins als Variablen hinterlegen
 
 void setup() {
   //ToDo Pin Deklarationen (evtl. automatisiert)
-  
+  tft.begin();
+  tft.fillScreen(TFT_RED);
 }
 
 void loop() {
   //Programmablauf einbauen
+  digitalWrite(25, HIGH);
 
 }
+
+//GRAFIKCHIP IST WAHRSCHEINLICH ILI9488
