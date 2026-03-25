477579133316b03ba2d130868b271eafbb23983f
DrRuxton <71925350+DrRuxton@users.noreply.github.com>
2026-03-25 18:08:12 +0100

    Kommunikationswechsel zu I2C kommunikation. Dedizierte Master und Slave environments

--- src/slave.cpp
+++ src/slave.cpp
@@ -1,7 +1,33 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-//definiere SCL und SDA pins
-#define SCL 22
-#define SDA 21
+#define I2C_DEV_ADDR 0x55
 
+uint32_t i = 0;
+
+void onRequest() {
+  Wire.write(String(i++).c_str());
+  Wire.write(" Packets.");
+  Serial.println("onRequest");
+  Serial.println();
+}
+
+void onReceive(int len) {
+  Serial.printf("onReceive[%d]: ", len);
+  while (Wire.available()) {
+    Serial.write(Wire.read());
+  }
+  Serial.println();
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.setDebugOutput(true);
+  Wire.onReceive(onReceive);
+  Wire.onRequest(onRequest);
+  Wire.begin((uint8_t)I2C_DEV_ADDR);
+}
+
+void loop(){
+  //nothing to see
+}
